2dfb2c53dc113d1c9917b4e3726c6742e96f3af1
Lyedson-sr <96959349+Lyedson-sr@users.noreply.github.com>
2023-12-13 21:04:33 +0000

    Initial commit

---
+++ case1.h
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "jogo-domino.h"
+
+void jogarDominos() {
+    printf("---------------\n");
+    mesa.Num_Pecas_Mesa = 0;
+    inicializarPecas();
+    embaralharPecas();
+    distribuirPecas();
+
+    quemComeca();
+    if (jogadores[0].Num_Pecas_Mao < jogadores[1].Num_Pecas_Mao) {
+      turno = 2;
+    }
+    doisJogadores();
+}
